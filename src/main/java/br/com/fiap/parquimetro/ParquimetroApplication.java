852d964019ac9c3225f74cbaa889cbeb2c7b48de
Eduardo J. Guerra <edujguerra@gmail.com>
2024-01-17 19:52:43 -0300

    versão 1.00.03 - já com Timer

--- src/main/java/br/com/fiap/parquimetro/ParquimetroApplication.java
+++ src/main/java/br/com/fiap/parquimetro/ParquimetroApplication.java
@@ -3,12 +3,27 @@ package br.com.fiap.parquimetro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 @SpringBootApplication
 public class ParquimetroApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ParquimetroApplication.class, args);
+
+		Timer timer = new Timer();
+
+		final long SEGUNDOS = 1000;
+		TimerTask tarefa = new TimerTask() {
+			@Override
+			public void run() {
+				System.out.println(new Date());
+			}
+		};
+
+		timer.scheduleAtFixedRate(tarefa, 0, SEGUNDOS);
 	}
 
 }
